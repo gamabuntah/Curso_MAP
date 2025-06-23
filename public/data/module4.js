1bab63f046b514d334b80c30f3eb5776daa576d8
Aluno PNSB <aluno@pnsb.com>
2025-06-23 10:18:06 -0300

    CORREÇÃO: Adicionado Mod 8.mp3 faltante e corrigidos módulos 1-3

--- public/data/module4.js
+++ public/data/module4.js
@@ -2,7 +2,7 @@ window.modulos_data = window.modulos_data || {};
 Object.assign(window.modulos_data, {
   "4": {
     "title": "Módulo 4: Captação, Condução e Infiltração (Bloco CCI)",
-    "audio": "MAP/Audios/Curso MAP - Mod 4.mp3",
+    "audio": null,
     "cards": [
       {
         "type": "default",
